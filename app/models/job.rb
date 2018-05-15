abe81a0697918fc898cf6dee32b50b09bef6cab1
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-15 17:44:39 -0600

    user can see comments in decending order, passes test

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,8 @@ class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   has_many :comments
+
+  def sort_comments
+    comments.order(created_at: :DESC)
+  end
 end
