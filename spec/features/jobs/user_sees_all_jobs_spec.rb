ee477a82425db9da8042ac2a4bc16fedee189965
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-16 12:25:56 -0600

    refactored link tests

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "User sees all jobs" do
-  scenario "a user sees all the jobs for a specific company" do
+  it "a user sees all the jobs for a specific company" do
     company = Company.create!(name: "ESPN")
     category = Category.create!(title: 'Developer')
 
@@ -13,5 +13,19 @@ describe "User sees all jobs" do
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("QA Analyst")
+  end
+
+  it "a user sees all the jobs for a specific company" do
+    category = Category.create!(title: 'Developer')
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: category.id)
+
+    visit company_jobs_path(company)
+
+    click_on "#{job.title}"
+
+
+    expect(current_path).to eq("/jobs/#{job.id}")
   end
 end
